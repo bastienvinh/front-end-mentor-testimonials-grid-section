8208775748295434fa7deb7712522d33fcd23a45
Bastien VINH <bvinh.dev@yahoo.com>
2023-02-07 20:47:44 +0100

    forget the alt on image

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -2,7 +2,7 @@ import "./Photo.scss"
 
 const Photo = ({ source }) => {
   return (
-    <img src={source} className="photo photo__rounded"  />
+    <img src={source} className="photo photo__rounded" alt="photo" />
   )
 }
 
